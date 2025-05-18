79dca3783daf6baa830266546afbb29d02b62047
Deekshant Yadav <d33kshant@gmail.com>
2025-05-18 17:20:23 +0530

    feat: moved column pipes to transformers

--- src/plmbr/transformers.py
+++ src/plmbr/transformers.py
@@ -57,3 +57,19 @@ class SortValues(Pipe):
 
     def flow(self, data: pd.DataFrame) -> pd.DataFrame:
         return data.sort_values(by=self.by, ascending=self.ascending)
+
+class DropColumns(Pipe):
+    def __init__(self, columns: list[str], name: str = "DropColumns"):
+        super().__init__(name)
+        self.columns = columns
+
+    def flow(self, data: pd.DataFrame) -> pd.DataFrame:
+        return data.drop(columns=self.columns) 
+
+class RenameColumns(Pipe):
+    def __init__(self, columns: dict[str, str], name: str = "RenameColumns"):
+        super().__init__(name)
+        self.columns = columns
+
+    def flow(self, data: pd.DataFrame) -> pd.DataFrame:
+        return data.rename(columns=self.columns)
