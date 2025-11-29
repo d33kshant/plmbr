6770e6cb916987d04035e5083928c459dce7dc90
Deekshant Yadav <d33kshant@gmail.com>
2025-11-29 20:39:43 +0530

    implemented core classes

--- src/plmbr/__init__.py
+++ src/plmbr/__init__.py
@@ -1,2 +1,17 @@
-def hello() -> str:
-    return "Hello from plmbr!"
+from abc import abstractmethod 
+import pandas as pd
+import typing as tp
+
+class Pipe:
+    @abstractmethod
+    def flow(self, data: pd.DataFrame) -> pd.DataFrame:
+        ...
+
+class Pipeline:
+    def __init__(self, pipes: tp.List[Pipe]):
+        self.pipes = pipes
+    
+    def flow(self, data: pd.DataFrame) -> pd.DataFrame:
+        for pipe in self.pipes:
+            data = pipe.flow(data)
+        return data
